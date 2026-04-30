0a8c4ce239cb41883c74036d12172a8731f29f82
DivergenteNM <nicolasmdivergent@gmail.com>
2026-04-29 22:37:46 -0500

    feat: configure server-side rendering for application routes in app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
+    path: 'my-projects/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: 'my-projects/:id/edit',
     renderMode: RenderMode.Server,
   },
